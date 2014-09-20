4ec56a2ce552308f37afb49c5b8a692eadc07d60
andi34 <skate4life@gmx.de>
2014-09-20 20:32:44 +0200

    Adapt Tab 3 changes:
    - update static busybox binary
    - update e2fsck
    - update su for Stock Rom to 2.2

--- ramdisk/res/misc/init.41/sbin/boot/install.sh
+++ ramdisk/res/misc/init.41/sbin/boot/install.sh
@@ -1,22 +1,44 @@
 VERSION=$(/sbin/busybox cat /res/misc/version.txt)
 
-if /sbin/busybox [ ! -f /system/blackhawk-next/release-$VERSION- ]; then
+set_perm() {
+  busybox chown $1.$2 $4
+  busybox chmod $3 $4
+}
+
+ch_con() {
+  busybox chcon u:object_r:system_file:s0 $1
+}
+
+if busybox [ ! -f /system/blackhawk-next/release-$VERSION- ]; then
   # Remount system RW
-  /sbin/busybox mount -o remount,rw /system
+  busybox mount -o remount,rw /system
 
   # Ensure /system/xbin exists
-  /sbin/busybox mkdir -p /system/xbin
-  /sbin/busybox chmod 755 /system/xbin
+  busybox mkdir -p /system/xbin
+  set_perm 0 0 0755 /system/xbin
 
   # Ensure /system/etc/init.d exists
-  /sbin/busybox mkdir -p /system/etc/init.d
-  /sbin/busybox chmod 755 /system/etc/init.d
+  busybox mkdir -p /system/etc/init.d
+  set_perm 0 0 0755 /system/etc/init.d
 
   # su
-  /sbin/busybox rm -f /system/bin/su
-  /sbin/busybox cp -f /res/misc/su /system/xbin/su
-  /sbin/busybox chown 0.0 /system/xbin/su
-  /sbin/busybox chmod 6755 /system/xbin/su
+  busybox rm -f /system/bin/su
+  busybox rm -f /system/xbin/su
+  busybox rm -f /system/bin/.ext/.su
+  # busybox rm -f /system/etc/install-recovery.sh
+
+  busybox mkdir -p /system/bin/.ext
+  busybox cp -f /res/misc/su /system/xbin/su
+  busybox cp -f /res/misc/su /system/bin/.ext/.su
+
+  set_perm 0 0 0777 /system/bin/.ext
+  set_perm 0 0 06755 /system/bin/.ext/.su
+  set_perm 0 0 06755 /system/xbin/su
+
+  ch_con /system/bin/.ext/.su
+  ch_con /system/xbin/su
+
+  /system/xbin/su --install
 
   # Superuser
   # /sbin/busybox rm -f /system/app/SuperUser.apk
@@ -41,24 +63,25 @@ if /sbin/busybox [ ! -f /system/blackhawk-next/release-$VERSION- ]; then
   cd /
   /sbin/busybox rm -f /data/property/persist.customboot.swap
 
+  # Ensure /system/lib/modules exists
+  busybox mkdir -p /system/lib/modules
+  set_perm 0 0 0755 /system/lib/modules
+  
   # Extended kernel modules
-  /sbin/busybox rm -f /system/etc/init.d/02modules
-  /sbin/busybox cp -f /res/misc/02modules /system/etc/init.d/02modules
-  /sbin/busybox chown 0.0 /system/etc/init.d/02modules
-  /sbin/busybox chmod 755 /system/etc/init.d/02modules
+  busybox rm -f /system/etc/init.d/02modules
+  busybox cp -f /res/misc/02modules /system/etc/init.d/02modules
+  set_perm 0 0 0755 /system/etc/init.d/02modules
 
   # CM userinit
-  /sbin/busybox rm -f /system/etc/init.d/90userinit
-  /sbin/busybox cp -f /res/misc/90userinit /system/etc/init.d/90userinit
-  /sbin/busybox chown 0.0 /system/etc/init.d/90userinit
-  /sbin/busybox chmod 755 /system/etc/init.d/90userinit
+  busybox rm -f /system/etc/init.d/90userinit
+  busybox cp -f /res/misc/90userinit /system/etc/init.d/90userinit
+  set_perm 0 0 0755 /system/etc/init.d/90userinit
   
   # Post boot script
-  /sbin/busybox rm -f /system/etc/init.sdcard.sh
-  /sbin/busybox rm -f /system/etc/init.post_boot.sh
-  /sbin/busybox cp -f /res/misc/post_boot.sh /system/etc/init.post_boot.sh
-  /sbin/busybox chown 0.0 /system/etc/init.post_boot.sh
-  /sbin/busybox chmod 750 /system/etc/init.post_boot.sh
+  busybox rm -f /system/etc/init.sdcard.sh
+  busybox rm -f /system/etc/init.post_boot.sh
+  busybox cp -f /res/misc/post_boot.sh /system/etc/init.post_boot.sh
+  set_perm 0 0 0750 /system/etc/init.post_boot.sh
 
   # Just in case
   # /sbin/busybox rm -f /system/etc/powervr.ini
@@ -67,33 +90,33 @@ if /sbin/busybox [ ! -f /system/blackhawk-next/release-$VERSION- ]; then
   # /sbin/busybox chmod 644 /system/etc/powervr.ini
 
   # Remove unnecessary PowerPV 3D binary if exists
-  /sbin/busybox rm -f /system/vendor/bin/hal_client_test
-  /sbin/busybox rm -f /system/vendor/bin/services_test_SGX540_120
-  /sbin/busybox rm -f /system/vendor/bin/sgx_render_flip_test_SGX540_120
-  /sbin/busybox rm -f /system/vendor/bin/sgx_init_test_SGX540_120
-  /sbin/busybox rm -f /system/vendor/bin/framebuffer_test
-  /sbin/busybox rm -f /system/vendor/bin/texture_benchmark
-  /sbin/busybox rm -f /system/vendor/bin/hal_server_test
-  /sbin/busybox rm -f /system/vendor/bin/testwrap
-  /sbin/busybox rm -f /system/vendor/bin/sgx_flip_test_SGX540_120
+  busybox rm -f /system/vendor/bin/hal_client_test
+  busybox rm -f /system/vendor/bin/services_test_SGX540_120
+  busybox rm -f /system/vendor/bin/sgx_render_flip_test_SGX540_120
+  busybox rm -f /system/vendor/bin/sgx_init_test_SGX540_120
+  busybox rm -f /system/vendor/bin/framebuffer_test
+  busybox rm -f /system/vendor/bin/texture_benchmark
+  busybox rm -f /system/vendor/bin/hal_server_test
+  busybox rm -f /system/vendor/bin/testwrap
+  busybox rm -f /system/vendor/bin/sgx_flip_test_SGX540_120
 
   # Force GPU rendering, user can change it anytime
-  /sbin/busybox echo -n true > /data/property/persist.sys.ui.hw
-  /sbin/busybox chmod 600 /data/property/persist.sys.ui.hw
+  busybox echo -n true > /data/property/persist.sys.ui.hw
+  set_perm 0 0 0600 /data/property/persist.sys.ui.hw
 
   # Remove old /data/property/persist.customboot.zram
   # Add compcache, zRAM swap per CPU core
-  /sbin/busybox rm -f /data/property/persist.customboot.zram
-  /sbin/busybox cp -f /res/misc/compcache /system/bin/compcache
-  /sbin/busybox chmod 755 /system/bin/compcache
+  busybox rm -f /data/property/persist.customboot.zram
+  busybox cp -f /res/misc/compcache /system/bin/compcache
+  set_perm 0 0 0755 /system/bin/compcache
 
   # Once be enough
-  /sbin/busybox mkdir -p /system/blackhawk-next
-  /sbin/busybox chmod 755 /system/blackhawk-next
-  /sbin/busybox rm /system/blackhawk-next/*
-  echo 1 > /system/blackhawk-next/release-$VERSION-
+  busybox mkdir -p /system/blackhawk-next
+  set_perm 0 0 0755 /system/blackhawk-next
+  busybox rm /system/blackhawk-next/*
+  busybox echo 1 > /system/blackhawk-next/release-$VERSION-
 
   # Remount system RO
-  /sbin/busybox sync
-  /sbin/busybox mount -o remount,ro /system
+  busybox sync
+  busybox mount -o remount,ro /system
 fi;
